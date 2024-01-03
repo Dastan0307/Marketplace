6b08a18a728e0c7040b558f9ee96f41dd5e50173
unknown <jylkychievdastan.0307@gmail.com>
2024-01-03 21:00:00 +0600

    add button register in main page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -17,12 +17,16 @@ const Header = ({ handleClickOpen }) => {
         <img src={logo} alt="Error :(" style={{ width: '196px', height: '44px' }} />
         <div className="header__btn">
             <PrimaryButton onClick={handleClickOpen}>Подать объявление</PrimaryButton>
-            <div className="header__user" onClick={() => navigate('/profile')} >
-                <p>{username ? username : ''}<br /><span>{email ? email : ''}</span></p>
+            <div className="header__user"  >
+                {
+                  username ? 
+                  <p onClick={() => navigate('/profile')}>{username ? username : ''}<br /><span>{email ? email : ''}</span></p> :
+                  <p onClick={() => navigate('/register')} style={{color: 'blue', marginRight: '40px'}}>Регистрация</p>
+                }
                 {
                   userImg ? 
-                  <img src={userImg} alt="Error :(" style={{width: '60px',height: '60px',  borderRadius: '100px'}} /> :
-                  <img src={headerIcon} alt="Error :(" style={{width: '60px'}} /> 
+                  <img src={userImg} alt="Error :(" onClick={() => navigate('/profile')} style={{width: '60px',height: '60px',  borderRadius: '100px'}} /> :
+                  <img src={headerIcon} alt="Error :(" onClick={() => navigate('/profile')} style={{width: '60px'}} /> 
                 }
             </div>
         </div>
